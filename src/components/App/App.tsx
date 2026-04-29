156eccdd0a490c2d952a0ef630267422de42094a
Roman <romario.higa@gmail.com>
2026-04-29 14:23:59 +0200

    componenst added

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,40 @@
 
-import './App.css'
+import css from './App.module.css'
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+import { useState } from 'react';
+import type { VoteType, Votes } from '../../types/votes';
 
 function App() {
-  
-
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
+  const handleVote = (type: VoteType) => {
+    setVotes(pre => ({
+      ...pre,
+      [type] :pre[type] + 1,
+    }))
+  };
+  const resetVotes = () => { setVotes({ good: 0, neutral: 0, bad: 0 }) };
+  const totalVotes = 0;
+  const positiveRate = 0;
   return (
-    <>
-      
-    </>
-  )
+    
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset ={true}
+      />
+      <VoteStats
+        votes={votes}
+        totalVotes={totalVotes}
+        positiveRate={positiveRate}
+      />
+      </div>
+    
+  );
 }
+
 
 export default App
